0bdbe55937f588046606009744b9382cdb1b8e63
Vlad <vladfriends@yahoo.ca>
2018-08-09 22:29:09 +0200

    Added movement forward, and debug line to show direction  of movement

--- Alligator AI Project/Assets/MovementController.cs
+++ Alligator AI Project/Assets/MovementController.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class MovementController : MonoBehaviour {
 
+    public float moveSpeed = 10f;
+
+    Vector3 movementDirection;
+
 	// Use this for initialization
 	void Start () {
-		
+        movementDirection = Vector3.right;
 	}
 	
 	// Update is called once per frame
-	void FixedUpdate () {
-		
+	void Update () {
+        transform.Translate(movementDirection * moveSpeed * Time.deltaTime);
+        Debug.DrawLine(transform.position, movementDirection * 100f, Color.red);
 	}
 }
